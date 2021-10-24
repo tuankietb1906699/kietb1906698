3ec79162995e7c68ecf834d46b90a5ee2106562d
tuankietb1906699 <kietb1906699@student.ctu.edu.vn>
2021-10-24 21:24:29 +0700

    hien thi danh sach lay ve tu server

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createWebHistory, createRouter } from "vue-router";
+const routes = [{
+    path: "/",
+    alias: "/contactbook",
+    name: "ContactBook",
+component: () => import("../views/ContactBook"),
+    },
+];
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+export default router;
